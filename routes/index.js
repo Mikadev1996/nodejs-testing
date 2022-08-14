0b6be3eb06464003c7764e927beb37b279cc8f7b
mika_laptop_windows <charmikadev@gmail.com>
2022-08-14 16:45:06 +0100

    index tests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const array = [];
+
+router.get("/", (req, res) => {
+  res.json({ name: "frodo" });
 });
 
+router.get("/test", (req, res) => res.json({ array }));
+
+router.post("/test", (req, res) => {
+  array.push(req.body.item);
+  res.send('success!');
+});
 module.exports = router;
